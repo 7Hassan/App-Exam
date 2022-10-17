6b716f6283187be7b1a065e978118223df5e29e2
7Hassan <egoker1234@gmail.com>
2022-10-18 00:49:25 +0200

    update v1.0.1

--- js/app.js
+++ js/app.js
@@ -4,21 +4,12 @@ let qestionNum = document.querySelector(".num-qestion").firstChild;
 let qestion = document.querySelector("h1");
 let inputs = [...document.querySelectorAll("input")];
 let answers = [...document.querySelectorAll(".label-answer")];
-let buttom = document.querySelector("button");
 let divQestion = document.querySelector(".qestion");
 let minutesSpan = document.querySelector(".min");
 let secoundSpan = document.querySelector(".sec");
 let counter = 0;
 let correctAnswer = 0;
 
-getData();
-
-// get data and set it in the page
-function getData() {
-  timer(2, 0);
-  setData(data, data.length);
-  click(data, data.length);
-}
 // set function to set data
 function setData(objectData, qestionsNumber) {
   qestionsAll.textContent = qestionsNumber;
@@ -30,19 +21,41 @@ function setData(objectData, qestionsNumber) {
     inputs[i].checked = false;
   }
 }
-// on click on buttom
-function click(objectData, qestionsNumber) {
-  buttom.addEventListener("click", function () {
-    if (counter === 8) {
-      end();
-    } else {
-      getAnswer(objectData);
-      counter++;
-      setData(objectData, qestionsNumber);
-    }
-  });
+
+// end the exam
+function end() {
+  divQestion.remove();
+  let divEnd = document.createElement("div");
+  divEnd.className = "end";
+  if (correctAnswer >= 6) {
+    let h2 = document.createElement("h2");
+    let span = document.createElement("span");
+    span.textContent = "♥";
+    span.style.color = "red";
+    let p = document.createElement("p");
+    h2.innerHTML = "Congratulations ";
+    h2.appendChild(span);
+    p.innerHTML = `${correctAnswer} <i class="fa-sharp fa-solid fa-circle-check"></i>`;
+    divEnd.appendChild(h2);
+    divEnd.appendChild(p);
+  } else {
+    let h3 = document.createElement("h3");
+    h3.textContent = "Sorry, try again.";
+    divEnd.appendChild(h3);
+  }
+  document.body.appendChild(divEnd);
 }
 
+// on click on buttom
+function clickButtom(objectData, qestionsNumber) {
+  if (counter === qestionsNumber - 1) {
+    end();
+  } else {
+    getAnswer(objectData);
+    counter++;
+    setData(objectData, qestionsNumber);
+  }
+}
 // get final answer
 function getAnswer(objectData) {
   for (var i = 0; i < answers.length; i++) {
@@ -53,7 +66,6 @@ function getAnswer(objectData) {
     }
   }
 }
-
 // set exam time
 function timer(minutes, secounds) {
   let showMen, showSec;
@@ -67,29 +79,19 @@ function timer(minutes, secounds) {
       minutes--;
       secounds = 59;
     }
-    if ((minutes === 0 && secounds < 0) || counter == 8) {
+    if ((minutes == 0 && secounds < 0) || counter == data.length - 1) {
       clearInterval(countDownTimer);
       return end();
     }
   }, 1000);
 }
-
-// end the exam
-function end() {
-  divQestion.remove();
-  let divEnd = document.createElement("div");
-  divEnd.className = "end";
-  if (correctAnswer >= 6) {
-    let h2 = document.createElement("h2");
-    let p = document.createElement("p");
-    h2.textContent = "Congratulations";
-    p.innerHTML = `${correctAnswer} <i class="fa-sharp fa-solid fa-circle-check"></i>`;
-    divEnd.appendChild(h2);
-    divEnd.appendChild(p);
-  } else {
-    let h3 = document.createElement("h3");
-    h3.textContent = "Sorry, try again.";
-    divEnd.appendChild(h3);
-  }
-  document.body.appendChild(divEnd);
+// hidden element
+function hiddenEle(ele) {
+  ele.id = "";
+}
+function readyButton(ele) {
+  hiddenEle(ele.parentElement);
+  divQestion.id = "show";
+  timer(2, 0);
+  setData(data, data.length);
 }
